2efbbf9425c94f637d440cc55eed3880207a1f6a
Julius van Dis <vandis.j@gmail.com>
2013-12-06 21:49:53 +0100

    Extended Solution and Ga
    Solution can now calculate the fitness for every fitness type. it can
    also return the maximum fitness
    The ga file now has complete tournaments, and solutions can mutate as
    well. Crossover is still left open, as well as structuring the
    population w.r.t. fitness.

--- Eclipse project/src/ga.java
+++ Eclipse project/src/ga.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -9,6 +10,7 @@ public class Ga{
 	private ArrayList population;
 	private int popSize;
 	private int tourSize;
+	
 	public Ga(int fitnessFunctionType,int populationSize, int tournamentSize){
 		
 		fitnessFunction = fitnessFunctionType;
@@ -19,25 +21,71 @@ public class Ga{
 	}
 	
 	private ArrayList performTournament(int tournamentSize){
+		ArrayList winners = new ArrayList();
 		switch (tournamentSize){
-		  case 1: 
+		  case 1:
+			  Random randomNr = new Random();
+			  for (int poolEntry=0;poolEntry<popSize;poolEntry++){
+				 winners.add(population.get(randomNr.nextInt(population.size())));
+			  }
 			  break;
 		  case 2:
+			  ArrayList tournPop = population;
+			  for (int round=1;round<3;round++){
+				System.out.println("The population before shuffling:");
+				System.out.println(tournPop.toString());
+				Collections.shuffle(tournPop);
+				System.out.println("The population after shuffling:");
+				System.out.println(tournPop.toString());
+				
+				for (int fighter1=0; fighter1<popSize-1; fighter1+=2){
+					if (((Solution)tournPop.get(fighter1)).getFitness() <= 
+						   ((Solution)tournPop.get(fighter1+1)).getFitness()){
+						winners.add(tournPop.get(fighter1+1));
+					} else {
+						winners.add(tournPop.get(fighter1));
+					}
+				}
+			  }
 			  break;
 		  default:
 			  break;
-			
 		}
-		return new ArrayList();
+		return winners;
 	}
 	
-	private void crossOver(){
-		
+	private void crossOver(Solution sol1,Solution sol2){
+		// what strategy?
 	}
 	
-	private void mutation(){
+	private void mutation(Solution sol){
+		Random randomNr = new Random();
+		float mutationGuess = randomNr.nextFloat();
+		int nrOfMutations=0;
+		while (mutationGuess < 1 / (2^(nrOfMutations+1) ) ){
+			nrOfMutations++;
+		}
 		
+		//Pick  random numbers!
+		int[] mutations = new int[nrOfMutations];
+		int nb_picked =0;
+		int index;
+		while (nb_picked < nrOfMutations){
+		   index = randomNr.nextInt(100);
+		   boolean found=false;
+		   for(int i=0;i<nb_picked;i++){
+			    if(mutations[i]==index){
+			    	found=true;
+			    	break;
+			    }
+		   }
+			  if(!found)
+				  mutations[nb_picked]=index;
+		}
+		sol.mutateSolution(mutations);
 	}
+		
+		// perform nrOfMutations number of mutations!
 	
 	public String runGa(int nrOfTimes,String answer){
 		if (nrOfTimes > 0) {
@@ -59,9 +107,9 @@ public class Ga{
 		     
 		     while(T<Integer.MAX_VALUE){
 		    	 performTournament(tourSize);
-		    	 crossOver();
-		    	 mutation();
-		    	 
+		    	 //crossOver();
+		    	 //mutation();
+		    	 //creation of children pool
 		     }
 		     
 			 /*   do while unchanged < 10*popsize or T=INTEGER.MAX_VALUE
--- Eclipse project/src/solution.java
+++ Eclipse project/src/solution.java
@@ -11,9 +11,13 @@ public class Solution{
 	
 	private int[] bitString; 
 	private int fitness;
+	private static int solLength;
+	private int functionType;
 	
 	public Solution(int[] bitStringSolution,int type){
 		bitString = bitStringSolution;
+		solLength = bitString.length;
+		functionType=type;
 		setFitness(bitString,type);
 		
 	}
@@ -25,24 +29,89 @@ public class Solution{
 		return fitness;
 	}
 	
-	public int getMaxFitness(int functionType){
+	public static int getMaxFitness(int functionType){
 		// Calculate max fitness for all cases! Then use a switch to report it back!
-		return 100;
+		switch(functionType){
+		   case 1:
+		   	   return solLength*1;
+		   case 2:
+			   return (solLength*(solLength+1))/2;
+		   case 3:
+		   case 4:
+			   return solLength/4*4;
+		   default:
+			   break;
+		}
+		return -1;
+	}
+	
+	
+	private static int calcFitness(int[] bitString, int functionType){
+		switch (functionType) {
+		case 1:  // uniformly scale counting ones function
+			int fit1 =0;
+			for (int bit=0;bit<bitString.length;bit++){
+				fit1 +=bitString[bit];
+			}
+			return fit1;
+			
+		case 2: // Linearly Scaled counting ones function
+			int fit2 =0;
+			for (int bit=0;bit<bitString.length;bit++){
+				fit2 +=bit*bitString[bit];
+			}
+			return fit2;
+			
+		case 3: // deceptive trap function k=4, d=1
+			int fit3=0;
+			int[] CO = new int[4];
+			int COfit;
+			for (int bit=0;bit<bitString.length;bit+=4){
+				CO[0] = bitString[bit];  CO[1]=bitString[bit+1];
+				CO[2]=bitString[bit+2];  CO[3]=bitString[bit+3];
+				COfit = calcFitness(CO,1);
+				if(COfit==4){
+					fit3+=4;
+				} else {
+					fit3+= 3-COfit; //4-1-(4-1)/(4-1)*COfit;
+				}
+			}
+			return fit3;
+			
+		case 4: // non-deceptive trap function k=4, d=2.5
+			int fit4=0;
+			int[] CO2 = new int[4];
+			int CO2fit;
+			for (int bit=0;bit<bitString.length;bit+=4){
+				CO2[0] = bitString[bit];  CO2[1]=bitString[bit+1];
+				CO2[2]=bitString[bit+2];  CO2[3]=bitString[bit+3];
+				CO2fit = calcFitness(CO2,1);
+				if(CO2fit==4){
+					fit4+=4;
+				} else {
+					fit4+= 1.5 - 0.5*CO2fit; //4-2.5-(4-2.5)/(4-1)*CO2fit;
+				}
+			}
+			return fit4;
+		default:
+			break;
+		}
+		
+		return -1; // if no correct function was set, return -1		
 	}
 	
 	public void setFitness(int[] bitString,int functionType){
-		switch (functionType) {
-			case 1:  // uniformly scale counting ones function
-				break;
-			case 2: // Linearly Scaled counting ones function
-				break;
-			case 3: // deceptive trap function
-				break;
-			case 4: // non-deceptive trap function
-				break;
-			default:
-				break;
-		}
+		fitness = calcFitness(bitString, functionType);
 	}
 	
+	public void mutateSolution(int[] indices){
+		for (int i=0;i<indices.length;i++){
+			if (bitString[i]==0){
+				bitString[i]=1;
+			} else {
+				bitString[i]=0;
+			}
+		}
+		setFitness(bitString,functionType);
+	}
 }
